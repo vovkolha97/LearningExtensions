6eaaa03ae39e902ec7be4575e006b81c45222bb3
Вовк Ольга <vovkolga@macbook-vovk.local>
2019-04-05 14:02:13 +0300

    Initial Commit

---
+++ extensionsDanceParty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  extensionsDanceParty
+//
+//  Created by Вовк Ольга' on 4/5/19.
+//  Copyright © 2019 Вовк Ольга'. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
