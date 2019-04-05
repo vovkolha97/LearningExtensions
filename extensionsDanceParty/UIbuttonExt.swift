ae5baebaab8562f1f227755021aa873a78518e86
Вовк Ольга <vovkolga@macbook-vovk.local>
2019-04-05 15:15:20 +0300

    Learning extensions

---
+++ extensionsDanceParty/UIbuttonExt.swift
@@ -0,0 +1,45 @@
+//
+//  UIbuttonExt.swift
+//  extensionsDanceParty
+//
+//  Created by Вовк Ольга' on 4/5/19.
+//  Copyright © 2019 Вовк Ольга'. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func wiggle() {
+        let wiggleAnim = CABasicAnimation(keyPath: "position")
+        wiggleAnim.duration = 0.05
+        wiggleAnim.repeatCount = 5
+        wiggleAnim.autoreverses = true
+        wiggleAnim.fromValue = CGPoint(x: self.center.x - 4.0, y: self.center.y)
+        wiggleAnim.toValue = CGPoint(x: self.center.x + 4.0, y: self.center.y)
+        layer.add(wiggleAnim, forKey: "position")
+        
+    }
+    
+    func dim() {
+        UIView.animate(withDuration: 0.15, animations: {
+            self.alpha = 0.75
+        }) { (finished) in
+            UIView.animate(withDuration: 0.15, animations: {
+                self.alpha = 1.0
+            })
+        }
+        
+    }
+    
+    func colorize() {
+        let randomNumberArray = generateRandomnumbers(quantity: 3)
+        let randomColor = UIColor(red: randomNumberArray[0]/255, green: randomNumberArray[1]/255, blue: randomNumberArray[2]/255, alpha: 1.0)
+        UIView.animate(withDuration: 0.3) {
+            self.backgroundColor = randomColor
+        }
+    }
+    
+    
+    
+}
--- extensionsDanceParty/ViewController.swift
+++ extensionsDanceParty/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorizeBtn: UIButton!
+    
+    @IBOutlet weak var wiggleBtn: UIButton!
+    
+    @IBOutlet weak var dimBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func colorizeBtnWasPressed(_ sender: Any) {
+        colorizeBtn.colorize()
+    }
+    
+    @IBAction func wiggleBtnWasPressed(_ sender: Any) {
+        wiggleBtn.wiggle()
+    }
+    @IBAction func dimBtnWasPressed(_ sender: Any) {
+        dimBtn.dim()
+    }
 }
 
